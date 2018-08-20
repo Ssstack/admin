d982611dc5051142c4612f0298cb0f237bf01ba8
小明 <xiaoming@qq.com>
2018-08-20 23:37:19 +0800

    登录页面完成

---
+++ src/views/Login.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="login">
+        <el-form class="container" :model="myForm" :rules="rules" ref="myForm">
+            <el-form-item>
+                <div>
+                    <img src="../assets/logo.jpg" class="avatar">
+                </div>
+            </el-form-item>
+            <el-form-item label="" prop="username">
+                <el-input prefix-icon="el-icon-edit" v-model="myForm.username" placeholder="账户">用户名</el-input>
+            </el-form-item>
+            <el-form-item label="" prop="password">
+                <el-input prefix-icon="el-icon-view" v-model="myForm.password" placeholder="密码">密码</el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button class="login-btn" type="primary">登录</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            myForm: {
+                username: '',
+                password: ''
+            },
+            rules: {
+                username: [
+                    { required: true, message: '请输入用户名', trigger: 'blur' },
+                    { min: 2, max: 10, message: '长度在 2 到 10 个字符', trigger: 'blur' }
+                ],
+                password: [
+                    { required: true, message: '请输入密码', trigger: 'blur' },
+                    { min: 2, max: 8, message: '长度在 2 到 8 个字符', trigger: 'blur' }
+                ]
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.login {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background-color: #2f4050;
+
+  .container {
+    position: absolute;
+    left: 0;
+    right: 0;
+    width: 400px;
+    padding: 0px 40px 15px 40px;
+    margin: 200px auto;
+    background: white;
+    .avatar {
+      position: relative;
+      left: 50%;
+      width: 120px;
+      height: 120px;
+      margin-left: -60px;
+      margin-top: -60px;
+      box-sizing: border-box;
+      border-radius: 50%;
+      border: 10px solid #fff;
+      box-shadow: 0 1px 5px #ccc;
+      overflow: hidden;
+    }
+    .login-btn {
+      width: 100%;
+    }
+  }
+}
+</style>
+
