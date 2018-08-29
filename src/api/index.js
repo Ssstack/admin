c046c5e70c3743b08862019baf5228c7b16996e5
小明 <xiaoming@qq.com>
2018-08-29 20:16:50 +0800

    用户列表完善

--- src/api/index.js
+++ src/api/index.js
@@ -1,49 +1,68 @@
-import axios from 'axios'
+import axios from "axios";
 
-const baseURL = "http://localhost:8888/api/private/v1/"
-axios.defaults.baseURL = baseURL
+const baseURL = "http://localhost:8888/api/private/v1/";
+axios.defaults.baseURL = baseURL;
 
 // 添加请求拦截器
-axios.interceptors.request.use(function (config) {
+axios.interceptors.request.use(
+  function(config) {
     // Do something before request is sent
-    let token = localStorage.getItem('myToken')
+    let token = localStorage.getItem("myToken");
     // 如果有token证明已经登录过,除了登录接口,其他所有请求请求头必须带Authorization=token(前后端约定)
-    if(token) {
-        config.headers['Authorization'] = token
+    if (token) {
+      config.headers["Authorization"] = token;
     }
-    return config
-}, function (error) {
+    return config;
+  },
+  function(error) {
     // Do something with request error
-    return Promise.reject(error)
-});
-
+    return Promise.reject(error);
+  }
+);
 
 // 登录验证
 export const checkUser = params => {
-    return axios.post('login',params).then(res => res.data)
-}
+  return axios.post("login", params).then(res => res.data);
+};
 
 // 获取用户数据列表
 export const getUserList = params => {
-    return axios.get('users', params).then(res => res.data)
-}
+  return axios.get("users", params).then(res => res.data);
+};
 
 // 修改用户状态
 export const changeUserState = params => {
-    return axios.put(`users/${params.uid}/state/${params.type}`, params).then(res => res.data)
-}
+  return axios
+    .put(`users/${params.uid}/state/${params.type}`, params)
+    .then(res => res.data);
+};
 
 // 添加用户
 export const addUser = params => {
-    return axios.post('users', params).then(res => res.data)
-}
+  return axios.post("users", params).then(res => res.data);
+};
 
 // 根据id获取用户信息
 export const getUserById = params => {
-    return axios.get(`users/${params}`).then(res => res.data)
-}
+  return axios.get(`users/${params}`).then(res => res.data);
+};
 
 // 编辑用户信息
 export const editUser = params => {
-    return axios.put(`users/${params.id}`, params).then(res => res.data)
+  return axios.put(`users/${params.id}`, params).then(res => res.data);
+};
+
+// 删除用户信息
+export const deleteUser = params => {
+  return axios.delete(`users/${params}`).then(res => res.data)
+}
+
+// 查询用户角色
+export const getRoleList = params => {
+  return axios.get(`roles`).then(res => res.data)
+}
+
+// 分配用户角色
+export const grantRole = params => {
+  return axios.put(`users/${params.id}/role`, {id: params.id, rid: params.rid}).then(res => res.data)
 }
--- src/views/user/User.vue
+++ src/views/user/User.vue
@@ -22,7 +22,7 @@
         <el-row>
             <el-col :span="24">
                 <template>
-                    <el-table :data="userList" border style="width: 100%">
+                    <el-table :data="userList" border style="width: 100%" v-loading="loading">
                         <el-table-column type="index" width="50">
                         </el-table-column>
                         <el-table-column prop="username" label="姓名" width="180">
@@ -39,8 +39,8 @@
                         <el-table-column label="操作" width="240">
                             <template slot-scope="scope">
                                 <el-button size="mini" icon="el-icon-edit" type="primary" plain @click="showEditDialog(scope.row)"></el-button>
-                                <el-button size="mini" icon="el-icon-delete" type="danger" plain></el-button>
-                                <el-button size="mini" icon="el-icon-check" type="success" plain></el-button>
+                                <el-button size="mini" icon="el-icon-delete" type="danger" plain @click="showDeleteDialog(scope.row)"></el-button>
+                                <el-button size="mini" icon="el-icon-check" type="success" plain @click="showGrantDialog(scope.row)"></el-button>
                             </template>
                         </el-table-column> 
                     </el-table>
@@ -102,170 +102,262 @@
                 <el-button type="primary" @click="editUserSubmit('editForm')">确 定</el-button>
             </div>
         </el-dialog>
+        <!-- 修改用户角色 -->
+        <el-dialog title="分配角色" :visible.sync="grantFormVisible">
+            <el-form :model="grantForm" :rules="rules" ref="grantForm" >
+                <el-form-item label="当前的用户：">
+                    <el-tag type="info">{{grantForm.username}}</el-tag>
+                </el-form-item>
+                <el-form-item label="请选择角色：">
+                    <el-select v-model="roleId" placeholder="请选择角色">
+                        <el-option v-for="(role, index) in roleList" :key="index" :label="role.roleName" :value="role.id"></el-option>
+                    </el-select>
+                    <!-- {{roleId}} -->
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="grantFormVisible = false">取 消</el-button>
+                <el-button type="primary" @click="grantUserSubmit()">确 定</el-button>
+            </div>
+        </el-dialog>
     </div>
 </template>
 <script>
-import { getUserList, changeUserState, addUser, getUserById, editUser } from '@/api';
+import {
+  getUserList,
+  changeUserState,
+  addUser,
+  getUserById,
+  editUser,
+  deleteUser,
+  getRoleList,
+  grantRole
+} from "@/api";
 export default {
-    data () {
-        return {
-            userList: [],
-            pagenum: 1,
-            pagesize: 4,
-            total: 0,
-            searchVal: '',
-            addFormVisible: false,
-            editFormVisible: false,
-            addForm: {
-                username: '',
-                password: '',
-                email: '',
-                mobile: ''
-            },
-            editForm: {
-                username: '',
-                email: '',
-                mobile: '',
-                id: 0
-            },
-            formLabelWidth: '100px',
-            rules: {
-                username: [
-                    { required: true, message: "请输入用户名", trigger: "blur" },
-                    { min: 2, max: 10, message: "长度在 2 到 10 个字符", trigger: "blur" }
-                ],
-                password: [
-                    { required: true, message: "请输入密码", trigger: "blur" },
-                    { min: 2, max: 8, message: "长度在 2 到 8 个字符", trigger: "blur" }
-                ],
-                email: [
-                    { required: true, message: "请输入邮箱地址", trigger: "blur" },
-                    { type: "email", message: "请输入正确的邮箱地址", trigger: "blur,change" }
-                ],
-                mobile: [
-                    { required: true, message: "请输入电话号码", trigger: "blur" },
-                    { min: 11, max: 11, message: "长度在 11 个字符", trigger: "blur" }
-                ]
-            }
-            
+  data() {
+    return {
+      loading: false,
+      userList: [],
+      pagenum: 1,
+      pagesize: 4,
+      total: 0,
+      searchVal: "",
+      addFormVisible: false,
+      editFormVisible: false,
+      grantFormVisible: false,
+      addForm: {
+        username: "",
+        password: "",
+        email: "",
+        mobile: ""
+      },
+      editForm: {
+        username: "",
+        email: "",
+        mobile: "",
+        id: 0
+      },
+      grantForm: {},
+      formLabelWidth: "100px",
+      roleList: [],
+      roleId: "",
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 2, max: 10, message: "长度在 2 到 10 个字符", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 2, max: 8, message: "长度在 2 到 8 个字符", trigger: "blur" }
+        ],
+        email: [
+          { required: true, message: "请输入邮箱地址", trigger: "blur" },
+          {
+            type: "email",
+            message: "请输入正确的邮箱地址",
+            trigger: "blur,change"
+          }
+        ],
+        mobile: [
+          { required: true, message: "请输入电话号码", trigger: "blur" },
+          { min: 11, max: 11, message: "长度在 11 个字符", trigger: "blur" }
+        ]
+      }
+    };
+  },
+  mounted() {
+    // 页面一加载，调用渲染数据列表的方法
+    this.renderList();
+  },
+  methods: {
+    renderList() {
+      // 加载动画
+      this.loading = true;
+      let params = {
+        params: {
+          // 页面一加载,this.searchVal为空
+          query: this.searchVal,
+          pagenum: this.pagenum,
+          pagesize: this.pagesize
         }
+      };
+      getUserList(params).then(res => {
+        // console.log(res)
+        if (res.meta.status === 200) {
+          this.userList = res.data.users;
+          this.total = res.data.total;
+          this.loading = false;
+        }
+      });
     },
-    mounted() {
-        // 页面一加载，调用渲染数据列表的方法
-        this.renderList()
+    queryUser() {
+      // 当用户输入关键词后，调用查询数据的方法
+      this.renderList();
     },
-    methods: {
-        renderList() {
-            let params = {
-                params: {
-                    // 页面一加载,this.searchVal为空
-                    query: this.searchVal,
-                    pagenum: this.pagenum,
-                    pagesize: this.pagesize
-                }
+    handleSizeChange(val) {
+      // console.log(`每页 ${val} 条`)
+      this.pagesize = val;
+      this.renderList();
+    },
+    handleCurrentChange(val) {
+      // console.log(`当前页: ${val}`)
+      this.pagenum = val;
+      this.renderList();
+    },
+    changeState(row) {
+      // console.log(row)
+      changeUserState({
+        uid: row.id,
+        type: row.mg_state
+      }).then(res => {
+        // console.log(res)
+        if (res.meta.status === 200) {
+          this.$message({
+            type: "success",
+            message: res.meta.msg
+          });
+        } else {
+          this.$message({
+            type: "error",
+            message: res.meta.msg
+          });
+        }
+      });
+    },
+    // 添加用户
+    addUserSubmit(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          addUser(this.addForm).then(res => {
+            if (res.meta.status === 201) {
+              this.$message({
+                message: res.meta.msg,
+                type: "success"
+              });
+              this.addFormVisible = false;
+              this.renderList();
             }
-            getUserList(params).then(res => {
-                // console.log(res)
-                if(res.meta.status === 200) {
-                    this.userList = res.data.users
-                    this.total = res.data.total
-                }
-            })
-        },
-        queryUser() {
-            // 当用户输入关键词后，调用查询数据的方法
-            this.renderList()
-        },
-        handleSizeChange(val) {
-            // console.log(`每页 ${val} 条`)
-            this.pagesize = val
-            this.renderList()
-        },
-        handleCurrentChange(val) {
-            // console.log(`当前页: ${val}`)
-            this.pagenum = val
-            this.renderList()
-        },
-        changeState(row) {
-            // console.log(row)
-            changeUserState({
-                uid: row.id,
-                type: row.mg_state
-            }).then(res => {
-                // console.log(res)
-                if(res.meta.status === 200) {
-                    this.$message({
-                        type: 'success',
-                        message: res.meta.msg
-                    })
-                }else {
-                    this.$message({
-                        type: 'error',
-                        message: res.meta.msg
-                    })
-                }
-            })
-        },
-        // 添加用户
-        addUserSubmit(formName) {
-            this.$refs[formName].validate((valid) => {
-                if(valid) {
-                    addUser(this.addForm).then(res => {
-                        if(res.meta.status === 201) {
-                            this.$message({
-                                message: res.meta.msg,
-                                type: 'success'
-                            })
-                            this.addFormVisible = false
-                            this.renderList()
-                        }
-                    })
-                }
-            })
-        },
-        // 展示编辑对话框
-        showEditDialog(row) {
-            console.log(row)
-            // this.editFormVisible = true
-            /* this.editForm =  {
+          });
+        }
+      });
+    },
+    // 展示编辑对话框
+    showEditDialog(row) {
+      console.log(row);
+      // this.editFormVisible = true
+      /* this.editForm =  {
                 username: row.username,
                 email: row.email,
                 mobile: row.mobile
             } */
-            getUserById(row.id).then(res => {
-                console.log(res)
-                this.editFormVisible = true
-                if(res.meta.status === 200) {
-                    this.editForm =  {
-                        username: res.data.username,
-                        email: res.data.email,
-                        mobile: res.data.mobile,
-                        id: res.data.id
-                    }
-                }
-            })
-        },
-        // 编辑用户信息提交
-        editUserSubmit (formName) {
-            this.$refs[formName].validate((valid) => {
-                if(valid) {
-                    editUser(this.editForm).then(res => {
-                        console.log(res)
-                        if(res.meta.status === 200) {
-                            this.editFormVisible = false
-                            this.$message({
-                                type: "success",
-                                message: res.meta.msg
-                            })
-                            this.renderList()
-                        }
-
-                    })
-                }
-            })
+      getUserById(row.id).then(res => {
+        console.log(res);
+        this.editFormVisible = true;
+        if (res.meta.status === 200) {
+          this.editForm = {
+            username: res.data.username,
+            email: res.data.email,
+            mobile: res.data.mobile,
+            id: res.data.id
+          };
         }
+      });
+    },
+    // 编辑用户信息提交
+    editUserSubmit(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          editUser(this.editForm).then(res => {
+            console.log(res);
+            if (res.meta.status === 200) {
+              this.editFormVisible = false;
+              this.$message({
+                type: "success",
+                message: res.meta.msg
+              });
+              this.renderList();
+            }
+          });
+        }
+      });
+    },
+    // 展示删除用户提示框
+    showDeleteDialog(row) {
+      this.$confirm("此操作将永久删除该用户, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          deleteUser(row.id).then(() => {
+            this.$message({
+              type: "success",
+              message: "删除成功!"
+            });
+            this.renderList();
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
+    },
+    // 展示修改用户角色提示框
+    showGrantDialog(row) {
+      this.grantFormVisible = true;
+      this.grantForm = row;
+      getRoleList().then(res => {
+        if (res.meta.status === 200) {
+          this.roleList = res.data;
+        }
+      });
+    },
+    // 分配角色
+    grantUserSubmit() {
+      // console.log(this.roleId)
+      if (!this.roleId) {
+        this.$message({
+          type: "error",
+          message: "角色不能为空"
+        });
+      } else {
+        grantRole({ id: this.grantForm.id, rid: this.roleId }).then(res => {
+          console.log(res);
+          if (res.meta.status === 200) {
+            this.$message({
+              type: "success",
+              message: res.meta.msg
+            });
+            this.grantFormVisible = false;
+            this.renderList();
+          }
+        });
+      }
     }
-}
+  }
+};
 </script>
 <style lang="scss" scoped>
 .user {
@@ -278,5 +370,3 @@ export default {
   }
 }
 </style>
-
-
